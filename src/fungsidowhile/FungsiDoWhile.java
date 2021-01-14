af9a20d2bce59a565ac83b052bf31c9d35fa2b31
Nurendra-bagaskara <nurendra_bagaskara_29rpl@student.smktelkom-mlg.sch.id>
2021-01-14 14:10:11 +0700

    sip

---
+++ src/fungsidowhile/FungsiDoWhile.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fungsidowhile;
+
+public class FungsiDoWhile {
+
+    public static void main(String[] args) {
+        // Deklarasi dan inisialisasi Variabel
+        int bil = 1;
+        //Perulangan
+        do {
+        // statement
+            System.out.println(bil+" ");
+            bil++;
+        }while ( bil <=10 );
+    }
+    
+}
